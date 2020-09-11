43f70d2bf8b37a09833d4bebeb573cf5707d91c3
Rakesh Arunachalam <rakesh@thoughtbot.com>
2020-09-11 19:37:03 +0100

    Update portfolio details

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,18 +1,63 @@
-import { useRouter } from 'next/router'
 
-const Details = ({ query, someVal }) => {
-    const router = useRouter()
+import React, { useState, useEffect } from 'react';
+import { useQuery, useLazyQuery } from '@apollo/react-hooks';
+import { GET_PORTFOLIO } from '@/apollo/queries';
+
+const PortfolioDetail = ({ query }) => {
+    const [portfolio, setPortfolio] = useState(null);
+    const [getPortfolio, { loading, data }] = useLazyQuery(GET_PORTFOLIO);
+
+    useEffect(() => {
+        getPortfolio({ variables: { id: query.id } })
+    }, [])
+
+    if (data && !portfolio) { setPortfolio(data.portfolio) }
+    if (loading || !portfolio) { return 'Loading...' };
 
     return (
-        <div>
-            {`Portfolio details ${router.query.id}, ${query.id} and ${someVal}`}
+        <div className="portfolio-detail">
+            <div className="container">
+
+                <div className="jumbotron">
+                    <h1 className="display-3">{portfolio.title}</h1>
+                    <p className="lead">{portfolio.jobTitle}</p>
+                    <p>
+                        <a className="btn btn-lg btn-success" href={portfolio.companyWebsite} role="button">
+                            See Company</a>
+                    </p>
+                </div>
+
+                <div className="row marketing">
+                    <div className="col-lg-6">
+                        <h4 className="title">Location</h4>
+                        <p className="text">{portfolio.location}</p>
+
+                        <h4 className="title">Start Date</h4>
+                        <p className="text">{portfolio.startDate}</p>
+                    </div>
+
+                    <div className="col-lg-6">
+                        {/* TODO: days later... */}
+                        <h4 className="title">Days</h4>
+                        <p className="text">44</p>
+
+                        <h4 className="title">End Date</h4>
+                        <p className="text">{portfolio.endDate}</p>
+                    </div>
+                    <div className="col-md-12">
+                        <hr />
+                        <h4 className="title">Description</h4>
+                        <p>{portfolio.description}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
 
-Details.getInitialProps = ({ query }) => ({
-    query,
-    someVal: 'Rak'
-})
+PortfolioDetail.getInitialProps = async ({ query }) => {
+    return { query };
+}
 
-export default Details
+
+export default PortfolioDetail;

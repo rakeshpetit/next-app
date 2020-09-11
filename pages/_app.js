e0e640c414ae963e7bfa8d3e6016a425d978e2f3
Rakesh Arunachalam <rakesh@thoughtbot.com>
2020-09-11 19:33:51 +0100

    Use apollo-boost and hooks for graphql client

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,42 @@
+
+
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import 'isomorphic-unfetch';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/index.scss';
 
 import Nav from '@/components/NavBar';
 import Hero from '@/components/Hero';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql'
+})
+
 const MyApp = ({ Component, pageProps }) => {
+
+  const isHomePage = () => Component.name === 'Home'
+
   return (
-    <div className="portfolio-app">
-      <Nav />
-      {Component.name === 'Home' && <Hero />}
-      <div className="container">
-        <Component {...pageProps} />
+    <ApolloProvider client={client}>
+      <div className="portfolio-app">
+        <Nav />
+        {isHomePage() && <Hero />}
+        <div className="container">
+          <Component {...pageProps} />
+        </div>
+        {/* FOOTER STARTS */}
+        {isHomePage() &&
+          <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
+            <div className="container text-center">
+              <small>Copyright &copy; Your Website</small>
+            </div>
+          </footer>
+        }
+        {/* FOOTER ENDS */}
       </div>
-    </div>
+    </ApolloProvider>
   )
 }
 
--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,56 +1,169 @@
-const Portfolios = () => (
-    <>
-        <div className="container">
+
+import { useState } from 'react';
+import axios from 'axios'
+import PortfolioCard from '@/components/portfolios/PortfolioCard';
+import Link from 'next/link';
+
+const graphDeletePortfolio = (id) => {
+    const query = `
+    mutation DeletePortfolio {
+      deletePortfolio(id: "${id}")
+    }
+  `
+
+    return axios.post('http://localhost:3000/graphql', { query })
+        .then(({ data: graph }) => graph.data)
+        .then(data => data.deletePortfolio)
+}
+
+const graphUpdatePortfolio = (id) => {
+    const query = `
+    mutation UpdatePortfolio {
+      updatePortfolio(id: "${id}",input: {
+        title: "UPDATE Job"
+        company: "UPDATE Company"
+        companyWebsite: "UPDATE Website"
+        location: "UPDATE Location"
+        jobTitle: "UPDATE Job Title"
+        description: "UPDATE Desc"
+        startDate: "12/12/2012 UPDATE"
+        endDate: "14/11/2013 UPDATE"
+      }) {
+        _id,
+        title,
+        company,
+        companyWebsite
+        location
+        jobTitle
+        description
+        startDate
+        endDate
+      }
+    }`;
+    return axios.post('http://localhost:3000/graphql', { query })
+        .then(({ data: graph }) => graph.data)
+        .then(data => data.updatePortfolio)
+}
+
+const graphCreatePortfolio = () => {
+    const query = `
+    mutation CreatePortfolio {
+      createPortfolio(input: {
+        title: "New Job"
+        company: "New Company"
+        companyWebsite: "New Website"
+        location: "New Location"
+        jobTitle: "New Job Title"
+        description: "New Desc"
+        startDate: "12/12/2012"
+        endDate: "14/11/2013"
+      }) {
+        _id,
+        title,
+        company,
+        companyWebsite
+        location
+        jobTitle
+        description
+        startDate
+        endDate
+      }
+    }`;
+    return axios.post('http://localhost:3000/graphql', { query })
+        .then(({ data: graph }) => graph.data)
+        .then(data => data.createPortfolio)
+}
+
+const fetchPortfolios = () => {
+    const query = `
+    query Portfolios {
+      portfolios {
+        _id,
+        title,
+        company,
+        companyWebsite
+        location
+        jobTitle
+        description
+        startDate
+        endDate
+      }
+    }`;
+    return axios.post('http://localhost:3000/graphql', { query })
+        .then(({ data: graph }) => graph.data)
+        .then(data => data.portfolios)
+}
+
+const Portfolios = ({ data }) => {
+    const [portfolios, setPortfolios] = useState(data.portfolios);
+
+    const createPortfolio = async () => {
+        const newPortfolio = await graphCreatePortfolio();
+        const newPortfolios = [...portfolios, newPortfolio];
+        setPortfolios(newPortfolios);
+    }
+
+    const updatePortfolio = async (id) => {
+        const updatedPortfolio = await graphUpdatePortfolio(id);
+        const index = portfolios.findIndex(p => p._id === id);
+        const newPortfolios = portfolios.slice();
+        newPortfolios[index] = updatedPortfolio;
+        setPortfolios(newPortfolios);
+    }
+
+    const deletePortfolio = async (id) => {
+        const deletedId = await graphDeletePortfolio(id);
+        const index = portfolios.findIndex(p => p._id === deletedId);
+        const newPortfolios = portfolios.slice();
+        newPortfolios.splice(index, 1);
+        setPortfolios(newPortfolios);
+    }
+
+    return (
+        <>
             <section className="section-title">
                 <div className="px-2">
                     <div className="pt-5 pb-4">
                         <h1>Portfolios</h1>
                     </div>
                 </div>
+                <button
+                    onClick={createPortfolio}
+                    className="btn btn-primary">Create Portfolio</button>
             </section>
             <section className="pb-5">
                 <div className="row">
-                    <div className="col-md-4">
-                        <div className="card subtle-shadow no-border">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                <p className="card-text fs-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                            <div className="card-footer no-border">
-                                <small className="text-muted">Last updated 3 mins ago</small>
-                            </div>
+                    {portfolios.map(portfolio =>
+                        <div key={portfolio._id} className="col-md-4">
+                            <Link
+                                href='/portfolios/[id]'
+                                as={`/portfolios/${portfolio._id}`}>
+                                <a className="card-link">
+                                    <PortfolioCard portfolio={portfolio} />
+                                </a>
+                            </Link>
+                            <button
+                                className="btn btn-warning"
+                                onClick={() => updatePortfolio(portfolio._id)}>Update Portfolio</button>
+                            <button
+                                onClick={() => deletePortfolio(portfolio._id)}
+                                className="btn btn-danger">
+                                Delete Portfolio
+              </button>
                         </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card subtle-shadow no-border">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                            <div className="card-footer no-border">
-                                <small className="text-muted">Last updated 3 mins ago</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card subtle-shadow no-border">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                            <div className="card-footer no-border">
-                                <small className="text-muted">Last updated 3 mins ago</small>
-                            </div>
-                        </div>
-                    </div>
+                    )
+                    }
                 </div>
             </section>
-            <a href="" className="btn btn-main bg-blue ttu">See More Portfolios</a>
-        </div>
-    </>
-)
+        </>
+    )
+}
 
-export default Portfolios
+Portfolios.getInitialProps = async () => {
+    const portfolios = await fetchPortfolios();
+    return { data: { portfolios } };
+}
+
+
+
+export default Portfolios;
